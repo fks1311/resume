eaba964a71c899495bd1e1731003b51ac838f77d
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2024-07-02 14:49:37 +0900

    global > default page 수정

--- src/global/DefaultPage.jsx
+++ src/global/DefaultPage.jsx
@@ -1,10 +1,24 @@
 import { forwardRef } from "react";
 import { styled } from "styled-components";
 
-const DefaultPage = forwardRef((props, ref) => {
-  return <PageContainer>DefaultPage</PageContainer>;
+const DefaultPage = forwardRef(({ children }, ref) => {
+  return (
+    <PageContainer className="page">
+      <ViewContainer className="view">{children}</ViewContainer>
+    </PageContainer>
+  );
 });
 
-const PageContainer = styled.div``;
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 1rem;
+`;
+const ViewContainer = styled.div`
+  width: inherit;
+  max-width: 1200px;
+`;
 
 export default DefaultPage;
