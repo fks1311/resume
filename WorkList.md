6d6afba7741f058b36a420ae1dc6eb9b22c7d5e1
ranyoung.kim <58300117+fks1311@users.noreply.github.com>
2024-07-05 17:44:45 +0900

    resume 파일 저장 방식 변경 및 작업 리스트 md 파일 생성

---
+++ WorkList.md
@@ -0,0 +1,7 @@
+# RESUME
+
+### list
+
+- [ ] Github token
+- [ ] Github 파일 동적 생성
+- [ ] Github 파일 데이터 저장 axios 테스트
--- src/pages/my_profile/My_profile.jsx
+++ src/pages/my_profile/My_profile.jsx
@@ -1,32 +1,10 @@
-import { MDXProvider } from "@mdx-js/react";
-import { styled } from "styled-components";
 import DefaultPage from "components/global/DefaultPage";
-import ME from "mdx/me.mdx";
+import ProfileTemplate from "components/profile/ProfileTemplate";
 
 export default function My_profile() {
   return (
     <DefaultPage>
-      <ViewContainer>
-        <MDXProvider>
-          <ME />
-        </MDXProvider>
-      </ViewContainer>
+      <ProfileTemplate />
     </DefaultPage>
   );
-}
-
-const ViewContainer = styled.form`
-  width: inherit;
-  max-width: 960px;
-  padding: 1rem;
-  border: 1px solid #eeedeb;
-  border-radius: 10px;
-`;
-
-{
-  /* <DefaultPage>
-  <MDXProvider>
-    <ME />
-  </MDXProvider>
-</DefaultPage>; */
 }
